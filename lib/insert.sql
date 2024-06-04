a6150f8e192f15b861ae76f22b71e7e673a571dd
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-06-04 06:36:50 +0300

    all commands accomplished

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id,name,age,sex,color,temperament,alive)
+VALUES (1,'Mr. Chocolate', 10, 'M', 'GREY','Aggressive', true),
+       (2,'Rowdy', 12, 'M', 'BROWN','Playful, Aggressive', true),
+       (3,'Tabitha', 5, 'F', 'BLACK', 'Calm, Curious', true),
+       (4,'Sergeant Brown', 7, 'M', 'BROWN','Calm, Intelligent', true),
+       (5,'Melissa', 3, 'F', 'WHITE','Playful, Curious', true),
+       (6,'Grinch', 1, 'M', 'BROWN','Aggressive', true),
+       (7,'Wendy', 2, 'F', 'BLACK','Calm, Intelligent', true),
+       (8,NULL, 12, "F", "GREY", "MOODY", false);
